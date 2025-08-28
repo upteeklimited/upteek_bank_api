4f54b7d2015bb067449da55e59c9ff35c30734fa
Jorn Kalu <kiceej@gmail.com>
2025-08-28 13:34:37 +0100

    Aug 28th 2025

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from fastapi_pagination import add_pagination
 import sys, traceback
 import os
 import redis.asyncio as redis
-from settings.config import load_env_config
 
 config = load_env_config()
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 sys.path.append(BASEDIR)
+
+from settings.config import load_env_config
 
 # Create a Redis connection pool (async)
 redis_client = redis.from_url(f"redis://:{config['redis_password']}@localhost:6379", decode_responses=True)
